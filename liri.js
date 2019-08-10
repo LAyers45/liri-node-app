f89567fac800f6d74facd416f2f89d10f1931b06
Sunny <loganayers2013@yahoo.com>
2019-08-10 00:42:34 -0500

    added initial switch cases

--- liri.js
+++ liri.js
@@ -3,3 +3,26 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
+
+var whichApi = process.argv[2];
+var whatInfo = process.argv[3];
+
+switch (whichApi) {
+    case "concert-this":
+
+        break;
+    case "spotify-this-song":
+
+        break;
+    case "movie-this":
+
+        break;
+    case "do-what-it-says":
+
+        break;
+
+    default:
+
+        break;
+
+}
