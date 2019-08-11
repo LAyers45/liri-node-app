d410ff4edc51ecc61ddac33bfab12b52150bb13b
Sunny <loganayers2013@yahoo.com>
2019-08-10 19:59:20 -0500

    updated the read me file

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # liri-node-app
+Liri is a node application that takes requests for concerts, songs, and or movies and returns information on them.
+
+The proper commands to utilize this app are as follows:
+'concert-this' <band name here>
+'spotify-this-song' <song title>
+'movie-this' <movie title>
+'do-what-it-says'
+
+This application uses BandsInTown API, Spotify API, OMDB API to find relevant information.
+
+replace the following in your own .env page with your own personal key, and secret key.
+-------------------------------------------
+# Spotify API keys
+
+SPOTIFY_ID=SPOTIFY-ID
+SPOTIFY_SECRET=SPOTIFY-SECRET_ID
+---------------------------------------------
--- liri.js
+++ liri.js
@@ -1,28 +1,37 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-
+var fs = require("fs");
+var Spotify = require("node-spotify-api")
 var spotify = new Spotify(keys.spotify);
-
+var request = require("request")
 var whichApi = process.argv[2];
-var whatInfo = process.argv[3];
+var whatInfo = process.argv.slice(3).join(" ");
 
 switch (whichApi) {
     case "concert-this":
-
+        concertThis();
         break;
     case "spotify-this-song":
-
+        spotifyThis();
         break;
     case "movie-this":
-
+        movieThis();
         break;
     case "do-what-it-says":
+        liriGo();
+        break;
+
+    default: console.log("\nType one of these after 'node liri.js': \nconcert-this <artist/band name here>\nspotify-this-song <song title>\nmovie-this <movie title>\ndo-what-it-says")
 
         break;
 
-    default:
+}
 
-        break;
+function concertThis() {
+    var bandSearch = "https://rest.bandsintown.com/artists/" + whatInfo + "/events?app_id=codingbootcamp";
 
+    request(bandSearch, function (error, response, body) {
+
+    })
 }
